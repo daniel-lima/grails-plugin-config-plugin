dd2edd21688afd0a7def5c6f4e9bce581a40277c
Burt Beckwith <burt@burtbeckwith.com>
2013-04-10 15:39:33 -0400

    bumped min Grails version to 1.3

--- PluginConfigGrailsPlugin.groovy
+++ PluginConfigGrailsPlugin.groovy
@@ -19,7 +19,7 @@ class PluginConfigGrailsPlugin {
     // the plugin version
     def version = '0.1.5'
     // the version or versions of Grails the plugin is designed for
-    def grailsVersion = '1.2.0 > *'
+    def grailsVersion = '1.3 > *'
     // the other plugins this plugin depends on
     def dependsOn = ['core': '* > 1.0']
 
