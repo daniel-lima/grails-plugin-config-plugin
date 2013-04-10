da1578df541282954a509c2c289e694c23f2f3f3
Burt Beckwith <burt@burtbeckwith.com>
2013-04-10 16:09:02 -0400

    cleanup

--- test/unit/org/plugin/config/ConfigObjectProxyTests.groovy
+++ test/unit/org/plugin/config/ConfigObjectProxyTests.groovy
@@ -1,67 +1,60 @@
 package org.plugin.config
 
-import org.grails.plugin.config.AbstractConfigHelper;
-import org.springframework.jmx.export.assembler.AbstractConfigurableMBeanInfoAssembler;
+import grails.test.GrailsUnitTestCase
+import grails.util.Environment
 
-import grails.test.*
-import grails.util.Environment;
+import org.grails.plugin.config.AbstractConfigHelper
 
 class ConfigObjectProxyTests extends GrailsUnitTestCase {
-    
-    void testUncheckedMap() {        
+
+    void testUncheckedMap() {
         ConfigObject config = loadConfig('PluginConfigDefaultConfig')
         Map configAsMap = AbstractConfigHelper.ConfigObjectProxy.newInstance(config, false)
-        
+
         assertNotNull configAsMap
         assertTrue configAsMap instanceof Map
         assertTrue configAsMap instanceof Writable
-        
+
         assertNull configAsMap.groovy
         assertFalse configAsMap.containsKey('groovy')
         assertNotNull configAsMap.grails
-        
+
         assertEquals 'configValue', configAsMap.grails.plugins.config.sampleValue2
         assertEquals config.grails.plugins.config.sampleValue2, configAsMap.grails.plugins.config.sampleValue2
-        
+
         assertNull configAsMap.grails.plugins.config.sampleValue5
         assertFalse configAsMap.grails.plugins.config.containsKey('sampleValue5')
         configAsMap.grails.plugins.config.sampleValue5 = 'xyz'
         assertEquals 'xyz', configAsMap.grails.plugins.config.sampleValue5
         assertEquals config.grails.plugins.config.sampleValue5, configAsMap.grails.plugins.config.sampleValue5
-        
-        
+
         assertEquals 1, configAsMap.grails.plugins.config.sampleValue1
         assertEquals true, configAsMap.grails.plugins.config.autoDefault
     }
-    
-    
+
     void testCheckedMap() {
         ConfigObject config = loadConfig('PluginConfigDefaultConfig')
         Map configAsMap = AbstractConfigHelper.ConfigObjectProxy.newInstance(config, true)
-        
+
         assertNotNull configAsMap
         assertTrue configAsMap instanceof Map
         assertTrue configAsMap instanceof Writable
-        
+
         assertFalse configAsMap.containsKey('groovy')
-        try {
+
+        shouldFail(IllegalArgumentException) {
             configAsMap.groovy
-            fail()
-        } catch (IllegalArgumentException e) {
-            assertTrue true
         }
-         
+
         assertNotNull configAsMap.grails
-        assertNotNull config.groovy 
+        assertNotNull config.groovy
         assertNotNull configAsMap.groovy
     }
-    
-    
-    def ConfigObject loadConfig(String className) {
+
+    ConfigObject loadConfig(String className) {
         ClassLoader cl = Thread.currentThread().contextClassLoader ?: this.class.classLoader
         ConfigSlurper cs = new ConfigSlurper(Environment.current.name)
         ConfigObject config = cs.parse(cl.loadClass(className))
         return config
     }
-    
 }
