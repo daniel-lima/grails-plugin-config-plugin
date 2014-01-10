7008a84f3352d07fdff2195e1b20e2b5788b2c00
Ondrej Kvasnovsky <ondrej.kvasnovsky@gmail.com>
2014-01-10 13:35:21 +0100

    UrlMappings seems to be mandatory in a grails plugin.

---
+++ grails-app/conf/UrlMappings.groovy
@@ -0,0 +1,11 @@
+class UrlMappings {
+    static mappings = {
+        "/$controller/$action?/$id?" {
+            constraints {
+            }
+        }
+
+        "/"(view: "/index")
+        "500"(view: '/error')
+    }
+}
